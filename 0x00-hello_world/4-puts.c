a05acec9c7078635178be62f7fcc208a43805831
mazikuamos <amosmaziku123@yahoo.com>
2023-02-16 22:43:19 +0000

    C program that prints exactly "Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point function
+ * Return: Return Value 0
+ * puts - Print String to Standard output
+ */
+
+int main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
