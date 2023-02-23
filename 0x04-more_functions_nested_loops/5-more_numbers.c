cbdabb8f6b50943e3fdb0005b0e82867a44002a5
mazikuamos <amosmaziku123@yahoo.com>
2023-02-23 20:39:38 +0000

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,14 +14,8 @@ void more_numbers(void)
 		for (n = 0; n < 15; n++)
 		{
 			if (n > 9)
-			{
 				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			else
-			{
-				_putchar(n + '0');
-			}
+			_putchar(n % 10 + '0');
 		}
 		_putchar('\n');
 	}
