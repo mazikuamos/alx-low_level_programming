2ce2d92d391a5c6ca5665c7d36882776eab100f8
mazikuamos <amosmaziku123@yahoo.com>
2023-02-23 12:11:35 +0000

    Tasks 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <ctype.h> 
+
+/**
+ * _isupper - function return uppercase value
+ * isupper - function return uppercase character
+ * Return: Return value
+ * @c: argument on _isupper function
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c) > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
