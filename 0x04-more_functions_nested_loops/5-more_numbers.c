5b708e96067576cf698c8e4806ec0f753af20fcd
mazikuamos <amosmaziku123@yahoo.com>
2023-02-23 19:36:52 +0000

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - function print number 10 times
+ * _putch - function print numbers on screen
+ */
+
+void more_numbers(void)
+{
+	int n, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+			else
+			{
+				_putchar(n + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
