a216bc9811a386b134917a6fcb287f2835219b17
mazikuamos <amosmaziku123@yahoo.com>
2023-02-23 14:59:24 +0000

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function multiple 2 numbers
+ * @a: argument within mul function
+ * @b: argument within mul function
+ * Return: Return value
+ */
+
+int mul(int a, int b)
+{
+	int value = a * b;
+
+	return (value);
+}
