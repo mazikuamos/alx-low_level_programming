81f467ab9685a9a89126cb4129efe75dde65753e
mazikuamos <amosmaziku123@yahoo.com>
2023-02-28 15:58:46 +0000

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - function prints half of a string
+ * @str: Input
+ * _putchar - function print on stdio
+ */
+
+void puts_half(char *str)
+{
+	int a, n, longi;
+
+	longi = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
+
+	n = longi / 2;
+
+	if (longi % 2 == 1)
+		n = ((longi + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+}
