88c36bf7a73b3734ae980228862d794c48feb33b
mazikuamos <amosmaziku123@yahoo.com>
2023-02-28 06:57:31 +0000

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function return len of string
+ * @s: Formal parameter
+ * Return: Return len
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}	
+	return (count);
+}
