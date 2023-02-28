92fcbc07fbacf59ac18f6ad8a1d635ffcf15b856
mazikuamos <amosmaziku123@yahoo.com>
2023-02-28 07:34:39 +0000

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts - function to print string on stdio
+ * @str: Formal parameter
+ * puts - function take pointer print to stdio
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
