662534ae1586fd988755008bedbdc11610821b6a
mazikuamos <amosmaziku123@yahoo.com>
2023-03-07 21:02:52 +0000

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memcpy - function copy element
+ * @dest: distination point
+ * @src: source of element
+ * @n: size of bytes copied
+ *
+ * Return: Return pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		dest[i + 8] = src[i];
+	}
+	return (dest);
+}
