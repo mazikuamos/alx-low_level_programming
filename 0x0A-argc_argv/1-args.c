968c85778fef49ccfb2e1536cb8ae29d1436c0d2
mazikuamos <amosmaziku123@yahoo.com>
2023-03-10 22:50:48 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of all codes
+ * @argc: size
+ * @argv: array argument
+ *
+ * Return: Return null
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
